67fc31bc7b4f4d49ee78b127a2585c09411741f1
jakubpietrusiak <jakub.pietrusiak@gmail.com>
2019-07-30 22:30:46 +0200

    ok

--- lista_gosci.py
+++ lista_gosci.py
@@ -1,9 +1,27 @@
+#lista gości
 guest = []
 
-for x in range(5):
+#pętla uzupełniająca listę
+for x in range(3):
     x=guest.append(input("Podaj imię osoby, którą chciałbyś zaprosić na przyjęcie : "))
 
+print("Super! Poniżej lista zaproszonych osób! \n")
+#wypisanie osób zaproszonych
+for x in guest:
+    print("Wysłałeś zaproszenie do :"+x)
 
-print(guest)
+#cofnięcie zaproszenia dla jednego z gości
+print("Oooo Nie! " + guest[2] + " nie może przyjść :( \n")
+absent=guest[2]
+guest.remove(absent)
+
+#zaktualizowana lista
+for x in guest:
+    print("Na przyjęcie przyjdzie "+x)
+
+#dopraszanie
+print("\n")
+print("No przecież zapomniałeś o najważniejszej osobie!!! Na pewno wiesz już o kogo chodzi :P ")
+#guest.insert(input())
 
 
