3f90085f51fe132f8571f91ea83c1db8e6a02dc8
jakubpietrusiak <jakub.pietrusiak@gmail.com>
2019-08-01 22:04:24 +0200

    nowe

---
+++ slownik_cwiczenia.py
@@ -0,0 +1,6 @@
+person = { 'first_name' : 'Oskar', 'last_name' : 'Pietrusiak', 'age' : 'dwa ' , 'city' : 'Siedlce'}
+
+print("Mam na imię " + person['first_name'].title() + ".")
+print(person['first_name'].title() + " " +person['last_name'].title())
+print("Mam " + person['age'].title() + "lata .")
+print("Moim rodzinnym miastem są " + person['city'].title() + ".")
