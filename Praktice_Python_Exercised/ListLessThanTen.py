38c0aa0e359aec34f617d1cea94656f9c388118b
jakubpietrusiak <jakub.pietrusiak@gmail.com>
2019-08-10 21:54:57 +0200

    wgrana poprawka

--- Praktice_Python_Exercised/ListLessThanTen.py
+++ Praktice_Python_Exercised/ListLessThanTen.py
@@ -12,6 +12,18 @@
 # list a that are smaller than that number given by the user.
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = []
 
 for elements in a:
-    print(elements <5)
+    if elements < 5:
+        b.append(elements)
+
+print(b)
+
+##############################################
+
+choice = int(input("Please give number :"))
+
+for elements in a :
+    if elements < choice:
+        print(elements)
