b22050f05a72c09aed59f141ad0f8798d8608b45
jakubpietrusiak <jakub.pietrusiak@gmail.com>
2020-03-03 21:43:50 +0100

    ol

---
+++ krotka.py
@@ -0,0 +1,13 @@
+#Cwiczenie 1 - krotki
+#Stwórz krotkę , w której będziesz przetrzymywał kilka danych. Za pomocą pętli for wyświetl je.
+
+autos = ('Audi','BMW','Mercedes','Volvo')
+
+for auto in autos:
+    print(auto)
+
+#nadpisanie krotki
+
+autos = ('Mazda','BMW','Honda','Volvo')
+for auto in autos:
+    print(auto)
